33d3a62519c070db10b5d0b66da86f5f3e9741d2
wlsdlf2 <wlsdlfzg@naver.com>
2025-03-03 20:29:39 +0900

    add Todo Entity

---
+++ src/main/java/com/example/todo/entity/Todo.java
@@ -0,0 +1,30 @@
+package com.example.todo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "todos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String task;
+    @Column(nullable = false)
+    private boolean completed = false;
+}
